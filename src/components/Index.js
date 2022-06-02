52a317d7301ef9bf03cdfa4e72727bf16a8e75c9
ttinonin <tinonin935@gmail.com>
2022-06-02 20:28:51 -0300

    Setting total expenses sum

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ExpenseList from "./ExpenseList";
 import ExpenseListFilters from "./ExpenseListFilter";
+import ExpensesSummary from "./ExpensesSummary";
 
 const Index = () => (
     <div>
+        <ExpensesSummary />
         <ExpenseListFilters />
         <ExpenseList />
     </div>
