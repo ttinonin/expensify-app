67df9c6819ad251437a1070d77c75e041caa82d9
ttinonin <tinonin935@gmail.com>
2022-06-02 19:38:10 -0300

    Configuring the date and amount display

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,11 +2,17 @@ import React from "react";
 import { connect } from "react-redux";
 import { removeExpense } from '../actions/expenses'
 import { Link } from "react-router-dom";
+import moment from "moment";
+import numeral from "numeral";
 
 const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
     <div>
         <Link to={`/edit${id}`}>{description}</Link>
-        <p>{amount} - {createdAt}</p>
+        <p>
+            {numeral(amount).format('$0,0.00')} 
+            -
+            {moment(createdAt).format('MMMM Do, YYYY')}
+        </p>
         <button onClick={()=>{
             dispatch(removeExpense({ id }))
         }}>Remove</button>
